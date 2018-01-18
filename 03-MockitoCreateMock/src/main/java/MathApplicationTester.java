b76e712d318b9be6c5d8068ea12eb9ced3cd6987
cproch <31203807+cproch@users.noreply.github.com>
2018-01-18 14:04:40 +0100

    Add files via upload
    Bis einschl. https://www.tutorialspoint.com/mockito/mockito_resetting_mock.htm

---
+++ 03-MockitoCreateMock/src/main/java/MathApplicationTester.java
@@ -0,0 +1,78 @@
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.*;
+
+// @RunWith attaches a runner with the test class to initialize the test data
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
+
+    private MathApplication mathApplication;
+    private CalculatorService calcService;
+
+    @Before
+    public void setUp() {
+        mathApplication = new MathApplication();
+        calcService = mock(CalculatorService.class);
+        mathApplication.setCalculatorService(calcService);
+    }
+
+    public void testAdd() {
+
+        //add the behavior to add numbers
+        when(calcService.add(20.0, 10.0)).thenAnswer(new Answer<Double>() {
+
+           // @Override
+            public Double answer(InvocationOnMock invocation) throws Throwable {
+                //get the arguments passed to mock
+                Object[] args = invocation.getArguments();
+
+                //get the mock
+                Object mock = invocation.getMock();
+
+                //return the result
+                return 30.0;
+            }
+        });
+
+        //test the add functionality
+        Assert.assertEquals(mathApplication.add(20.0, 10.0), 30.0, 0);
+    }
+
+    @Test
+    public void testAddAndSubtract() {
+
+        //add the behavior to add numbers
+        when(calcService.add(20.0, 10.0)).thenReturn(30.0);
+
+        //subtract the behavior to subtract numbers
+        when(calcService.subtract(20.0, 10.0)).thenReturn(10.0);
+
+        //test the subtract functionality
+        Assert.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0, 0);
+
+        //test the add functionality
+        Assert.assertEquals(mathApplication.add(20.0, 10.0), 30.0, 0);
+
+        //verify call to calcService is made or not
+    /*    verify(calcService).add(20.0, 10.0);
+        verify(calcService).subtract(20.0, 10.0);
+    */
+
+        //create an inOrder verifier for a single mock
+        InOrder inOrder = inOrder(calcService);
+
+        //following will make sure that add is first called then subtract is called.
+        inOrder.verify(calcService).subtract(20.0, 10.0);
+        inOrder.verify(calcService).add(20.0, 10.0);
+
+
+    }
+}
